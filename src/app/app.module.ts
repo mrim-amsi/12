6a514cf831282edcf4a438101b9f024276046dd3
mairram <merriamamsi@gmail.com>
2022-12-09 13:49:34 +0200

    scand commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/Home/home.component';
-import { FooterComponent } from './footer/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './haedar/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent, FooterComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
